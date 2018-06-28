af47a81981d27c5f3ad0b75918f4542285669cfa
xKoda <correodavid5@gmail.com>
2018-06-28 11:19:38 +0200

    Update 006.py

--- .vscode/006.py
+++ .vscode/006.py
@@ -10,4 +10,5 @@ for i in range(101):
 squaresum = squaresum * squaresum
 difference = squaresum - sumsquare
 print(difference)
-
+# // alternate
+# print(sum([x for x in range(101)])**2 - sum([x*x for x in range(101)]))
